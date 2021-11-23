b30b257e7c6dae5d80cd3c6300d29369c230c18a
Finlay Cannon <fin@finlaycannon.com>
2021-11-22 20:57:08 -0800

    cleanup

--- 02.test.js
+++ 02.test.js
@@ -1,4 +1,5 @@
 // See https://adventofcode.com/2020/day/2
+
 import { filter, toInteger } from 'lodash';
 import raw_input from './input/input_02.txt';
 
@@ -10,45 +11,41 @@ const parse = (list) =>
 
 const countLetter = (letter, str) => str.split('').filter((chr) => chr === letter).length;
 
-const findValidEntries = (db) =>
-    filter(db, (entry) => {
-        const count = countLetter(entry.letter, entry.pass);
-        return entry.min <= count && count <= entry.max;
-    });
+const validEntry = (entry) => {
+    const count = countLetter(entry.letter, entry.pass);
+    return entry.min <= count && count <= entry.max;
+};
 
-const findValidEntries2 = (db) =>
-    filter(db, (entry) => {
-        const a = entry.pass[entry.min - 1];
-        const b = entry.pass[entry.max - 1];
-        return (a === entry.letter || b === entry.letter) && a !== b;
-    });
+const validEntry2 = (entry) => {
+    const a = entry.pass[entry.min - 1];
+    const b = entry.pass[entry.max - 1];
+    return (a === entry.letter || b === entry.letter) && a !== b;
+};
 
 describe('Day 2', () => {
     test('Example 1', () => {
-        const input = ['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc'];
-        const db = parse(input);
-        const valids = findValidEntries(db);
+        const db = parse(['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc']);
+        const valids = db.filter(validEntry);
         expect(valids.length).toBe(2);
     });
 
     test('Example 2', () => {
-        const input = ['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc'];
-        const db = parse(input);
-        const valids = findValidEntries2(db);
+        const db = parse(['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc']);
+        const valids = db.filter(validEntry2);
         expect(valids.length).toBe(1);
     });
 
     test('Part 1', () => {
         const input = raw_input.split('\n');
         const db = parse(input);
-        const valids = findValidEntries(db);
+        const valids = db.filter(validEntry);
         expect(valids.length).toBe(536);
     });
 
     test('Part 2', () => {
         const input = raw_input.split('\n');
         const db = parse(input);
-        const valids = findValidEntries2(db);
+        const valids = db.filter(validEntry2);
         expect(valids.length).toBe(558);
     });
 });
