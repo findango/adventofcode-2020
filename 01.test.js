8c7200d6b4b50a832782801e2d65b528687fc4f8
Finlay Cannon <fin@finlaycannon.com>
2021-11-22 20:58:11 -0800

    Day 1: Use itertools for permutations

--- 01.test.js
+++ 01.test.js
@@ -1,28 +1,13 @@
 // See https://adventofcode.com/2020/day/1
 
-import { toInteger } from 'lodash';
+import { sum, toInteger } from 'lodash';
+import { permutations } from 'itertools';
 import raw_input from './input/input_01.txt';
 
-const findPair = (total, list) => {
-    for (let i = 0; i < list.length; i++) {
-        for (let j = i + 1; j < list.length; j++) {
-            if (list[i] + list[j] === total) {
-                return [list[i], list[j]];
-            }
-        }
-    }
-    return [];
-};
-
-const findTriplet = (total, list) => {
-    for (let i = 0; i < list.length; i++) {
-        for (let j = i + 1; j < list.length; j++) {
-            for (let k = j + 1; k < list.length; k++) {
-                if (list[i] + list[j] + list[k] === total) {
-                    return [list[i], list[j], list[k]];
-                }
-            }
-        }
+const find = (count, total, list) => {
+    const groups = permutations(list, count);
+    for (const group of groups) {
+        if (sum(group) === total) return group;
     }
     return [];
 };
@@ -32,25 +17,25 @@ const multiply = (list) => list.reduce((p, c) => p * c, 1);
 describe('Day 1', () => {
     test('Example 1', () => {
         const input = [1721, 979, 366, 299, 675, 1456];
-        const pair = findPair(2020, input);
+        const pair = find(2, 2020, input);
         expect(multiply(pair)).toEqual(514579);
     });
 
     test('Example 2', () => {
         const input = [1721, 979, 366, 299, 675, 1456];
-        const triplet = findTriplet(2020, input);
+        const triplet = find(3, 2020, input);
         expect(multiply(triplet)).toEqual(241861950);
     });
 
     test('Part 1', () => {
         const input = raw_input.split(/\n/).map(toInteger);
-        const pair = findPair(2020, input);
+        const pair = find(2, 2020, input);
         expect(multiply(pair)).toEqual(1015476);
     });
 
     test('Part 2', () => {
         const input = raw_input.split(/\n/).map(toInteger);
-        const triplet = findTriplet(2020, input);
+        const triplet = find(3, 2020, input);
         expect(multiply(triplet)).toEqual(200878544);
     });
 });
